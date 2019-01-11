5ba9aea569a9710b220233360a9dd74ae8a0c308
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-11 17:07:31 -0200

    Tratamento padrão para outros erros com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,11 +2,12 @@ import { Observable } from 'rxjs/Rx';
 import { Injectable } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { StorageService } from '../services/storage.service';
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService) {
+    constructor(public storage: StorageService, public alertCtrl: AlertController) {
     }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -23,17 +24,46 @@ export class ErrorInterceptor implements HttpInterceptor {
             console.log(errorObj)
 
             switch (errorObj.status) {
+                case 401:
+                    this.handle401()
+                    break
                 case 403:
                     this.handle403()
                     break
+                default:
+                    this.handleDefaultError(errorObj)
             }
 
             return Observable.throw(errorObj)
         }) as any
     }
 
+    handle401() {
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'E-mail ou senha incorretos',
+            enableBackdropDismiss: false,
+            buttons: [
+                { text: 'Ok' }
+            ]
+        })
+        alert.present()
+    }
+
     handle403() {
         this.storage.setLocalUser(null)
+    }
+
+    handleDefaultError(errorObj) {
+        let alert = this.alertCtrl.create({
+            title: 'Erro ' + errorObj.status + ': ' + errorObj.error,
+            message: errorObj.message,
+            enableBackdropDismiss: false,
+            buttons: [
+                { text: 'Ok' }
+            ]
+        })
+        alert.present()
     }
 }
 
