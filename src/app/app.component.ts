44e0f7eb6b4408940e79197d51546dc61d985e10
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-11 15:51:35 -0200

    Criando uma página de profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,15 @@ export class MyApp {
 
   rootPage: string = 'HomePage';
 
-  pages: Array<{title: string, component: string}>;
+  pages: Array<{ title: string, component: string }>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: 'HomePage' }
+      { title: 'Profile', component: 'ProfilePage' },
+      { title: 'Categorias', component: 'CategoriasPage' },
     ];
 
   }
