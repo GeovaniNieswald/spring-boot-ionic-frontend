500a03ecb4de71f92f75d2453e1aff21c1973a6c
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-12 14:44:07 -0200

    Povoando dinamicamente estado e cidade

---
+++ src/services/domain/estado.service.ts
@@ -0,0 +1,16 @@
+import { API_CONFIG } from './../../config/api.config';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs/Rx';
+import { EstadoDTO } from '../../models/estado.dto';
+
+@Injectable()
+export class EstadoService {
+
+    constructor(public http: HttpClient) {
+    }
+
+    findAll(): Observable<EstadoDTO[]> {
+        return this.http.get<EstadoDTO[]>(`${API_CONFIG.baseUrl}/estados`)
+    }
+}
