ed51a5d5de6e99567479e797752bbc722f3351b0
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-13 16:45:52 -0200

    Tela de confirmação de pedido

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -27,6 +27,6 @@ export class PaymentPage {
 
   nextPage() {
     this.pedido.pagamento = this.formGroup.value
-    console.log(this.pedido)
+    this.navCtrl.setRoot('OrderConfirmationPage', { pedido: this.pedido })
   }
 }
