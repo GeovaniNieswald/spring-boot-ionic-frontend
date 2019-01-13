bbb54d38f9a4737a8605eb31bd600e34167e6bb2
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-13 15:20:43 -0200

    Buscando endereços do banco de dados

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,6 +1,6 @@
 import { StorageService } from './../storage.service';
 import { API_CONFIG } from './../../config/api.config';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs/Rx';
 import { ClienteDTO } from '../../models/cliente.dto';
@@ -11,8 +11,8 @@ export class ClienteService {
     constructor(public http: HttpClient, public storage: StorageService) {
     }
 
-    findByEmail(email: string): Observable<ClienteDTO> {
-        return this.http.get<ClienteDTO>(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`)
+    findByEmail(email: string) {
+        return this.http.get(`${API_CONFIG.baseUrl}/clientes/email?value=${email}`)
     }
 
     getImageFromBucket(id: string): Observable<any> {
