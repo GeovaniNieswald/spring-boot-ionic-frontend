ee9d01725d0c0e45e959666ba6bc465fc879e8d4
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-13 17:46:40 -0200

    Infinite scroll

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -11,7 +11,8 @@ import { API_CONFIG } from '../../config/api.config';
 })
 export class ProdutosPage {
 
-  items: ProdutoDTO[]
+  items: ProdutoDTO[] = []
+  page: number = 0
 
   constructor(
     public navCtrl: NavController,
@@ -27,17 +28,19 @@ export class ProdutosPage {
   loadData() {
     let categoria_id = this.navParams.get('categoria_id')
     let loader = this.presentLoading()
-    this.produtoService.findByCategoria(categoria_id).subscribe(response => {
-      this.items = response['content']
+    this.produtoService.findByCategoria(categoria_id, this.page, 10).subscribe(response => {
+      let start = this.items.length
+      this.items = this.items.concat(response['content'])
+      let end = this.items.length - 1
       loader.dismiss()
-      this.loadImageUrls()
+      this.loadImageUrls(start, end)
     }, error => {
       loader.dismiss()
     })
   }
 
-  loadImageUrls() {
-    for (var i = 0; i < this.items.length; i++) {
+  loadImageUrls(start: number, end: number) {
+    for (var i = start; i <= end; i++) {
       let item = this.items[i]
 
       this.produtoService.getSmallImageFromBucket(item.id).subscribe(response => {
@@ -59,10 +62,21 @@ export class ProdutosPage {
   }
 
   doRefresh(refresher) {
+    this.page = 0
+    this.items = []
+
     this.loadData()
 
     setTimeout(() => {
       refresher.complete()
+    }, 1000)
+  }
+
+  doInfinite(infiniteScroll) {
+    this.page++
+    this.loadData()
+    setTimeout(() => {
+      infiniteScroll.complete()
     }, 1000)
   }
 }
