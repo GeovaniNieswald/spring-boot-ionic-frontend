204db63280398f741d83f95d51271edda1dc9bf8
GeovaniNieswald <geovaninieswald@gmail.com>
2019-01-13 14:24:49 -0200

    Terminando as funcionalidades do carrinho

--- src/pages/cart/cart.ts
+++ src/pages/cart/cart.ts
@@ -1,3 +1,4 @@
+import { ProdutoDTO } from './../../models/produto.dto';
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CartItem } from '../../models/cart-item';
@@ -35,5 +36,25 @@ export class CartPage {
         item.produto.imageUrl = `${API_CONFIG.bucketBaseUrl}/prod${item.produto.id}-small.jpg`
       }, error => { })
     }
+  }
+
+  removeItem(produto: ProdutoDTO) {
+    this.items = this.cartService.removeProduto(produto).items
+  }
+
+  increaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.increaseQuantity(produto).items
+  }
+
+  decreaseQuantity(produto: ProdutoDTO) {
+    this.items = this.cartService.decreaseQuantity(produto).items
+  }
+
+  total(): number {
+    return this.cartService.total()
+  }
+
+  goOn() {
+    this.navCtrl.setRoot('CategoriasPage')
   }
 }
--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,13 +5,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { CredenciaisDto } from "../models/credenciais.dto";
 import { JwtHelper } from "angular2-jwt";
+import { CartService } from './domain/cart.service';
 
 @Injectable()
 export class AuthService {
 
     jwtHelper: JwtHelper = new JwtHelper()
 
-    constructor(public http: HttpClient, public storage: StorageService) {
+    constructor(
+        public http: HttpClient,
+        public storage: StorageService,
+        public cartService: CartService) {
     }
 
     authenticate(creds: CredenciaisDto) {
@@ -29,6 +33,7 @@ export class AuthService {
             email: this.jwtHelper.decodeToken(tok).sub
         }
         this.storage.setLocalUser(user)
+        this.cartService.createOrClearCart()
     }
 
     logout() {
